78008fc2f37ee4961f6fc7edfacc872c57928486
AdibZikri580 <s70426@ocean.umt.edu.my>
2025-06-24 12:18:30 +0800

    Update DBUtil.java

--- src/java/util/DBUtil.java
+++ src/java/util/DBUtil.java
@@ -2,12 +2,16 @@ package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBUtil {
 
-    public static Connection getConnection() throws Exception {
+    private static final String URL = "jdbc:mysql://mainline.proxy.rlwy.net:31869/railway?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "imsdAgIUZwcdPfXZlfPXuxfnHffdyblU";
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(
-            "mysql://root:imsdAgIUZwcdPfXZlfPXuxfnHffdyblU@mainline.proxy.rlwy.net:31869/railway", "root", "imsdAgIUZwcdPfXZlfPXuxfnHffdyblU");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
